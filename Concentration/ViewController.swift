34dc497c7e8b1a612aabd6070866abffd575c0b7
Timmy <timothy.chan489@gmail.com>
2018-04-21 19:20:01 -0400

    V2 commit.  See change log for changes

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         var flipCard1 = "HELLO";
         var flipCount = 0;
     */
-    var flipCount = 0 {
+    var flipCount: Int = 0 {
         //Property observer
         //Everytime flipCount is changed, the label will be updated.
         //Keeps UI in sync with instance variables.
@@ -21,23 +21,35 @@ class ViewController: UIViewController {
             flipCountLabel.text = "Flips: \(flipCount)";
         }
     }
- 
-    //Outlet creates an instance variable.
-    //UILabel cannot be inferred.  Has to be explicitly declared.
-    //Notice that there is no initialization and that there is no error.
+    /*
+        Outlet creates an instance variable.
+        UILabel cannot be inferred.  Has to be explicitly declared.
+        Notice that there is no initialization and that there is no error.
+    */
     @IBOutlet weak var flipCountLabel: UILabel!
-
+    /*
+        Outlet collection (An array of UIButton)
+        Nil means an optional is not set
+        An optional is indicated by a '!' and has 2 states: set and unset.
+     */
+    @IBOutlet var touchCards: [UIButton]!
+    
+    //Not necessary to put data_type of array but I am keeping it
+    var emoijiChoices : Array<String> = ["👻", "🎃", "🐝", "🐴"];
+    
     //This method is connected to first button on the storyboard
     @IBAction func touchButton(_ sender: UIButton) {
         //print("Arg, a ghost!");
         flipCount += 1;
-        flipCard(WithEmoji: "👻", on: sender);
-    }
-    
-    //This method is connected to second button on storyboard.
-    @IBAction func touchSecondButton(_ sender: UIButton) {
-        flipCount += 1;
-        flipCard(WithEmoji: "🎃", on: sender);
+        //Let defines constant.
+        //Putting '!' at end of an unset optional will crash app
+        //let cardNumber = touchCards.index(of: sender)!;
+        if let cardNumber : Int = touchCards.index(of: sender) {
+            flipCard(WithEmoji: emoijiChoices[cardNumber], on: sender)
+            print("CardNumber: = \(cardNumber)");
+        } else {
+            print("Chosen card is not in collection.");
+        }
     }
     
     /*
@@ -58,4 +70,3 @@ class ViewController: UIViewController {
         }
     }
 }
-
